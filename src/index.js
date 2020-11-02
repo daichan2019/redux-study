15cf628249689932a27be3aad65a3ce38dce81fb
daichi_sato <daichiexperience@gmail.com>
2020-11-02 22:30:09 +0900

    [Add]
    React-ReduxのチュートリアルをもとにTodo Appを作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import TodoApp from "./TodoApp";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
   document.getElementById("root")
 );
